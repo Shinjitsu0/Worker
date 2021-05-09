6c3e263b5a35ebad84f9157cbd728015e362169a
Vladimir Durov <Shinjitsu0@gmail.com>
2021-05-09 12:35:23 +0300

    first

---
+++ OnTaskErrorListener.java
@@ -0,0 +1,5 @@
+package worker;
+
+public interface OnTaskErrorListener {
+    void onError(String result);
+}
---
+++ Worker.java
@@ -0,0 +1,29 @@
+package worker;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if(i == 33){
+                errorCallback.onError("Task " + i + " is undone");
+            }
+            else{
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
